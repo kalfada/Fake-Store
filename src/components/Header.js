6ce031b9b845c800eca60e78fb2fcc7028a85d7a
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2022-01-14 15:26:43 +0200

    init react

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import '../style/Header.css'
+
+export default function Header() {
+    return (
+            <h1>Fake Store</h1>
+    )
+}
---
+++ src/components/Item.js
@@ -0,0 +1,10 @@
+export default function Item() {
+    return(
+        <div>
+            <img src="" alt="" />
+            <div>price</div>
+            <button>Add to Cart</button>
+            <p>Description</p>
+        </div>
+    )
+}
